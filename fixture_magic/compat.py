76dee825473ae3e72bfc6059f205e38b3fe34004
Andreas Halldén <andreas.hallden@allears.ai>
2025-11-10 15:38:22 +0100

    Add arg include_fk_fields

--- fixture_magic/compat.py
+++ fixture_magic/compat.py
@@ -1,8 +1,18 @@
-
-
-def get_all_related_objects(model, exclude_fields):
+def get_all_related_objects(model, exclude_fields, include_fields):
+    if include_fields:
+        return [
+            f
+            for f in model._meta.get_fields()
+            if (f.one_to_many or f.one_to_one)
+            and f.auto_created
+            and not f.concrete
+            and f.name in include_fields
+        ]
     return [
-        f for f in model._meta.get_fields() if
-        (f.one_to_many or f.one_to_one) and
-        f.auto_created and not f.concrete and f.name not in exclude_fields
+        f
+        for f in model._meta.get_fields()
+        if (f.one_to_many or f.one_to_one)
+        and f.auto_created
+        and not f.concrete
+        and f.name not in exclude_fields
     ]
